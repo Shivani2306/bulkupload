95a0323d9ea32587bd53805a0fba1fb134ea6261
Shivani2306 <129536470+Shivani2306@users.noreply.github.com>
2024-11-27 15:47:48 +0530

    Update upload.py

--- upload.py
+++ upload.py
@@ -65,7 +65,8 @@ def employee_image(id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=10000)
 
 
 
