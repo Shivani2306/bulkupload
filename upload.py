0f318f945ba4107907e7e9d210b88b31627e230c
Shivani2306 <129536470+Shivani2306@users.noreply.github.com>
2024-11-27 16:20:12 +0530

    Update upload.py

--- upload.py
+++ upload.py
@@ -66,7 +66,7 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     # app.run(debug=True)
-    app.run(debug=True, host="0.0.0.0", port=10000)
+    app.run(debug=True, host="0.0.0.0")
 
 
 
